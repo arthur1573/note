eb7393e530580281d4695960579b4b0787501cf0
tzlmzyt <arthursimpsons@outlook.com>
2022-04-26 12:55:03 +0800

    upload a note

---
+++ 220425.py
@@ -0,0 +1,44 @@
+# print("hello")
+
+
+
+
+
+
+# output = 0
+# print(f"1: {output}")
+
+# def help_me_get_the_answer():
+    # output = 3 * 2
+    # print(f"2: {output}")
+    # return output 
+# print(f"3: {output}")
+
+
+# help_me_get_the_answer()
+# print(f"4: {output}")
+
+
+# output = help_me_get_the_answer()
+# print(f"5: {output}")
+
+
+# output = "None"
+# print(f"6: {output}")
+
+
+# output = None
+# print(f"7: {output}")
+
+
+
+
+
+
+
+
+
+# output = help_me_get_the_answer()
+# print(output)
+
+
