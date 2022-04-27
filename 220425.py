29e741721f6d7ec20f98c01be15de8bb3e6edd7b
tzlmzyt <arthursimpsons@outlook.com>
2022-04-28 00:04:17 +0800

    added a apple scope

--- 220425.py
+++ 220425.py
@@ -3,8 +3,6 @@
 
 
 
-
-
 # output = 0
 # print(f"1: {output}")
 
@@ -35,10 +33,41 @@
 
 
 
-
-
-
 # output = help_me_get_the_answer()
 # print(output)
+
+
+
+
+
+
+
+
+# o = "OXFORD BOOKWORMS LIBRARY".title()
+# print(o)
+
+
+
+# print("built a ladder notes 21".title())
+
+
+
+
+# vs code could open these hint
+# not notepad++
+
+# def help_me_get_a_apple():
+    # """This function is going to help a human
+    # get a apple.hhhhhhhhhhhhh"""
+    # print("apple")
+
+# help_me_get_a_apple()
+
+
+
+
+
+
+
 
 
