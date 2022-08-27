ae6c869a9f5f026d2aed3251b3ba7c81041dbba0
tzlmzyt <arthursimpsons@outlook.com>
2022-08-27 10:44:46 +0800

    Auto-committed on date

--- note.py
+++ note.py
@@ -1996,4 +1996,280 @@
 
 
 
+########################## get()
+# class Node:
+    # def __init__(self, value):
+        # self.value = value
+        # self.next = None
+
+
+
+
+# class LinkedList:
+    # def __init__(self, value):
+        # new_node = Node(value)
+        # self.head = new_node
+        # self.tail = new_node
+        # self.length = 1
+
+
+
+
+    # def print_list(self):     
+        # temp = self.head
+        # while temp is not None:
+            # print(temp.value)
+            # temp = temp.next
+        # else:
+        # # "else:" equal to "if temp is None:" or "if temp == None:"
+            # print(temp)
+            
+        
+
+    
+    # def append(self, value):
+        # new_node = Node(value)
+        # if self.head is None:
+        # # if self.length == 0:
+            # self.head = new_node
+            # self.tail = new_node
+        # else: 
+            # self.tail.next = new_node
+            # self.tail = new_node
+        # self.length += 1
+
+
+    # def get(self, index):
+        # if index < 0 or index >= self.length:
+            # return None
+        # temp = self.head
+        # for _ in range(index):
+            # temp = temp.next
+        # return temp
+
+
+# my_linked_list = LinkedList(0)
+# my_linked_list.append(1)
+# my_linked_list.append(2)
+# my_linked_list.append(3)
+
+# my_linked_list.print_list()
+# print()
+
+# print(my_linked_list.get(2).value)
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+##################################### set_value
+# class Node:
+    # def __init__(self, value):
+        # self.value = value
+        # self.next = None
+
+
+
+
+# class LinkedList:
+    # def __init__(self, value):
+        # new_node = Node(value)
+        # self.head = new_node
+        # self.tail = new_node
+        # self.length = 1
+
+
+
+
+    # def print_list(self):     
+        # temp = self.head
+        # while temp is not None:
+            # print(temp.value)
+            # temp = temp.next
+        # else:
+        # # "else:" equal to "if temp is None:" or "if temp == None:"
+            # print(temp)
+            
+        
+
+    # def append(self, value):
+        # new_node = Node(value)
+        # if self.head is None:
+        # # if self.length == 0:
+            # self.head = new_node
+            # self.tail = new_node
+        # else: 
+            # self.tail.next = new_node
+            # self.tail = new_node
+        # self.length += 1
+
+
+
+    # def get(self, index):
+        # if index < 0 or index >= self.length:
+            # return None
+        # temp = self.head
+        # for _ in range(index):
+        # # "_" is not "i", also not going to print(i)
+            # temp = temp.next
+        # return temp
+
+
+
+
+    # def set_value(self, index, value):
+        # temp = self.get(index)
+        # # equal to "if index < 0 or index >= self.length: "
+        # if temp:
+        # # equal to "if temp is not None:"
+            # temp.value = value
+            # return True
+        # return False
+
+
+
+# my_linked_list = LinkedList(0)
+# my_linked_list.append(1)
+# my_linked_list.append(2)
+# my_linked_list.append(3)
+
+# my_linked_list.print_list()
+# print()
+
+
+# my_linked_list.set_value(0, 76)
+# my_linked_list.print_list()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ############################## insert()
+# class Node:
+    # def __init__(self, value):
+        # self.value = value
+        # self.next = None
+
+
+
+
+# class LinkedList:
+    # def __init__(self, value):
+        # new_node = Node(value)
+        # self.head = new_node
+        # self.tail = new_node
+        # self.length = 1
+
+
+
+
+    # def print_list(self):     
+        # temp = self.head
+        # while temp is not None:
+            # print(temp.value)
+            # temp = temp.next
+        # else:
+            # print(temp)
+            
+
+
+
+    # def append(self, value):
+        # new_node = Node(value)
+        # if self.head is None:
+        # # if self.length == 0:
+            # self.head = new_node
+            # self.tail = new_node
+        # else: 
+            # self.tail.next = new_node
+            # self.tail = new_node
+        # self.length += 1
+
+
+
+
+    # def prepend(self, value):
+        # new_node = Node(value)
+        # if self.length == 0:
+            # self.head = new_node
+            # self.tail = new_node
+        # else:
+            # new_node.next = self.head
+            # self.head = new_node
+        # self.length += 1
+        # return True
+
+
+
+
+    # def get(self, index):
+        # if index < 0 or index >= self.length:
+            # return None
+        # temp = self.head
+        # for _ in range(index):
+            # temp = temp.next
+        # return temp
+
+
+
+
+
+    # def insert(self, index, value):
+        # if index < 0 or index > self.length:
+            # return False
+        # if index == 0:
+            # return self.prepend(value)
+        # if index == self.length:
+            # return self.append(value)
+        # new_node = Node(value)
+        # temp = self.get(index - 1)
+        # new_node.next = temp.next
+        # temp.next = new_node
+        # self.length += 1
+        # return True
+
+
+
+# my_linked_list = LinkedList(0)
+# my_linked_list.append(1)
+# my_linked_list.append(2)
+# my_linked_list.append(3)
+
+# my_linked_list.print_list()
+# print()
+
+
+# my_linked_list.insert(1, 76)
+# my_linked_list.print_list()
+
+
+
+
+
+
+
+
+
+
+
+
 
