2b5b3b6590dffcaa99adbe524f9542f3de18a236
tzlmzyt <arthursimpsons@outlook.com>
2022-08-29 20:56:10 +0800

    Auto-committed on date

--- note.py
+++ note.py
@@ -663,6 +663,42 @@
 
 
 
+'''
+import note
+
+print("-----------------------------")
+print(" ")
+print("This will always be run 2.")
+
+
+print("The second module's name = {}".format(__name__))
+
+
+def main():
+    print(
+    """
+    This will NOT always be run. 
+    This module's name = {}
+    """
+    .format(__name__))
+    
+    
+ 
+
+if __name__ == "__main__":
+    main()
+else:
+    print("this module is run from import.")
+'''
+
+
+
+
+
+
+
+
+
 
 ################# print factorial number in human way
 # n = 10
